2d78ffb52dd7dfa488af2a4b8d8b15b774487939
Mark Schipper <schipperius@gmail.com>
2013-12-20 20:38:19 -0700

    initial git repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchipGraphic::Application.config.secret_key_base = '41ba4e7aaf4c6de1eb9615cdd69fa0ced4bfe81f818f4542613f2e688ecfc1ed4a0b460b8871efbc5e06e34f6cbe62d6f23f6aae9184b2068f2e1ea38daf8add'
