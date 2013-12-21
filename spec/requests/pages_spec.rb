d98aaf428989e99b103f1a883c831d0244f23026
Mark Schipper <schipperius@gmail.com>
2013-12-21 11:13:56 -0700

    Set up home page.

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 describe "Pages" do
   describe "Home page" do
-    it "should have the content 'Test;" do
+    it "should have the content 'Overview;" do
      visit '/pages/home'
       get pages_home_path
-      expect(page).to have_content('Test')
+      expect(page).to have_content('Overview')
+    end
+
+    it "should have the right title" do
+    	visit '/pages/home'
+    	expect(page).to have_title("Schipperius Infographic")
     end
   end
 end
